433a889351ca149601002ed1b63eaeb6bc916b72
CanKOKSAL <118839518+CanKOKSAL@users.noreply.github.com>
2022-12-19 20:07:56 +0400

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,10 +1,39 @@
-﻿Console.WriteLine("Plase write your Name and Surname");
-string name = Console.ReadLine();
-string Surname = Console.ReadLine();
+
+
+
+using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
+string name = "";
+string Surname = "";
+double score = 0;
+
+Console.WriteLine("Plase write your Name and Surname");
+name = Console.ReadLine();
+Surname = Console.ReadLine();
 
 Console.WriteLine("And your score");
-int score = int.Parse(Console.ReadLine());
+score = double.Parse(Console.ReadLine());
 
+do
+{
+    if(name.Length < 3 && Surname.Length < 3)
+    {
+     Console.WriteLine("Please write your name carefully");
+     name = Console.ReadLine();
+     Surname = Console.ReadLine();
+    }
+   
+} while (name.Length < 3 && Surname.Length < 3);
+
+
+do
+{
+    if(score > 100 && score < 0)
+    { 
+     Console.WriteLine("Please write true value");
+     score = double.Parse(Console.ReadLine());
+    }  
+} while (score > 100 && score < 0);
 
 Student student = new Student(name, Surname, score);
 
