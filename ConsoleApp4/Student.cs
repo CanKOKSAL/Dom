897375b40908de1370f1e10b1fbb006d67a98ac1
CanKOKSAL <118839518+CanKOKSAL@users.noreply.github.com>
2022-12-19 21:32:08 +0400

    Update Student.cs

--- ConsoleApp4/Student.cs
+++ ConsoleApp4/Student.cs
@@ -32,7 +32,35 @@ class Student : Person
         Console.WriteLine($"Name: {this.Name}");
         Console.WriteLine($"Surname: {this.SurName}");
         Console.WriteLine($"Score: {this.Score}");
-       
+
+        do
+        {
+            if (Name.Length < 3 || SurName.Length < 3)
+            {
+                Console.WriteLine("Please write your name and surname carefully");
+                Name = Console.ReadLine();               
+                SurName = Console.ReadLine();
+                Console.WriteLine("And write your value again");
+                Score = double.Parse(Console.ReadLine());
+            }
+
+        } while (Name.Length < 3 || SurName.Length < 3);
+
+
+        do
+        {
+            if (Score > 100 || Score < 0)
+            {
+                Console.WriteLine("Please write your name and surname again");
+                Name = Console.ReadLine();
+                SurName = Console.ReadLine();
+                Console.WriteLine("And write your true value");
+
+                Score = double.Parse(Console.ReadLine());
+            }
+        } while (Score > 100 || Score < 0);
+
+
         if (Score > 50.99)
         {
             this.IsGraduated = true;
