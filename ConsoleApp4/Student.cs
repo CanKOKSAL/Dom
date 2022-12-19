b13245a5bb51f3a658e2f219212aa77265f52834
CanKOKSAL <118839518+CanKOKSAL@users.noreply.github.com>
2022-12-19 20:08:31 +0400

    Update Student.cs

--- ConsoleApp4/Student.cs
+++ ConsoleApp4/Student.cs
@@ -1,18 +1,20 @@
-﻿using System.Xml.Linq;
+using System;
+using System.Xml.Linq;
 
 class Student : Person
 {
-    public int Score;
+    public double Score;
     public bool IsGraduated;
 
-    public Student(string name, string surname, int score)
+
+    public Student(string name, string surname, double score)
     {
         Name = name;
         SurName = surname;
         Score = score;
     }
 
-    public Student(string name, string surname, int score, bool isGraduated)
+    public Student(string name, string surname, double score, bool isGraduated)
     {
         Name = name;
         SurName = surname;
@@ -30,17 +32,18 @@ class Student : Person
         Console.WriteLine($"Name: {this.Name}");
         Console.WriteLine($"Surname: {this.SurName}");
         Console.WriteLine($"Score: {this.Score}");
-        if (Score > 50)
+       
+        if (Score > 50.99)
         {
             this.IsGraduated = true;
-           
-                Console.WriteLine($"Congrats!");
+
+            Console.WriteLine($"Congrats!");
         }
-        if (Score <= 79 && Score >= 51)
+        if (Score <= 79.99 && Score >= 51)
         {
             Console.WriteLine("You are eligible to Graduated");
         }
-        if (Score <= 89 && Score >= 80)
+        if (Score <= 89.99 && Score >= 80)
         {
             Console.WriteLine("You are eligible to Graduated as a Honor student");
         }
@@ -48,14 +51,14 @@ class Student : Person
         {
             Console.WriteLine("You are eligible to Graduated as a High Honor student");
         }
-        if( this.IsGraduated == false)
+        if (this.IsGraduated == false)
         {
-         Console.WriteLine("You are failed!");
+            Console.WriteLine("You are failed!");
         }
     }
     public void CanTakeExam()
     {
-        if (this.Score <= 89 && this.Score >= 80)
+        if (this.Score <= 89.99 && this.Score >= 80)
         {
             Console.WriteLine("And also you can reexam for make your Score higher");
         }
