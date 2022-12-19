b19b9a9e82f4e73e0be1c4042b18cb43abd382b6
CanKOKSAL <118839518+CanKOKSAL@users.noreply.github.com>
2022-12-19 21:31:46 +0400

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,6 +1,3 @@
-
-
-
 using System.Xml.Linq;
 using static System.Formats.Asn1.AsnWriter;
 string name = "";
@@ -14,28 +11,11 @@ Surname = Console.ReadLine();
 Console.WriteLine("And your score");
 score = double.Parse(Console.ReadLine());
 
-do
-{
-    if(name.Length < 3 && Surname.Length < 3)
-    {
-     Console.WriteLine("Please write your name carefully");
-     name = Console.ReadLine();
-     Surname = Console.ReadLine();
-    }
-   
-} while (name.Length < 3 && Surname.Length < 3);
-
-
-do
-{
-    if(score > 100 && score < 0)
-    { 
-     Console.WriteLine("Please write true value");
-     score = double.Parse(Console.ReadLine());
-    }  
-} while (score > 100 && score < 0);
 
 Student student = new Student(name, Surname, score);
 
 student.GetFullInfo();
 student.CanTakeExam();
+
+
+
